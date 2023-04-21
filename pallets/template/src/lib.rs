09e6777f6ccc1d757ac4c06381bddf892065a1e7
dharam-net <dharmendra.b@netxd.com>
2023-04-21 18:33:11 +0530

    added contract capability

---
+++ pallets/template/src/lib.rs
@@ -0,0 +1,166 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+/// Edit this file to define custom logic or remove it if it is not needed.
+/// Learn more about FRAME and the core library of Substrate FRAME pallets:
+/// <https://docs.substrate.io/reference/frame-pallets/>
+pub use pallet::*;
+use sp_core::{crypto::KeyTypeId};
+pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"demo");
+ub mod crypto {
+	use super::KEY_TYPE;
+	use sp_core::sr25519::Signature as Sr25519Signature;
+	use sp_runtime::{
+		app_crypto::{app_crypto, sr25519},
+		traits::Verify, MultiSignature, MultiSigner
+	};
+	app_crypto!(sr25519, KEY_TYPE);
+
+	pub struct TestAuthId;
+
+	// implemented for runtime
+	impl frame_system::offchain::AppCrypto<MultiSigner, MultiSignature> for TestAuthId {
+	type RuntimeAppPublic = Public;
+	type GenericSignature = sp_core::sr25519::Signature;
+	type GenericPublic = sp_core::sr25519::Public;
+	}
+}
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	/// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+	}
+
+	// The pallet's runtime storage items.
+	// https://docs.substrate.io/main-docs/build/runtime-storage/
+	#[pallet::storage]
+	#[pallet::getter(fn something)]
+	// Learn more about declaring storage items:
+	// https://docs.substrate.io/main-docs/build/runtime-storage/#declaring-storage-items
+	pub type Something<T> = StorageValue<_, u32>;
+
+	// Pallets use events to inform users when important changes are made.
+	// https://docs.substrate.io/main-docs/build/events-errors/
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// Event documentation should end with an array that provides descriptive names for event
+		/// parameters. [something, who]
+		SomethingStored { something: u32, who: T::AccountId },
+	}
+
+	// Errors inform users that something went wrong.
+	#[pallet::error]
+	pub enum Error<T> {
+		/// Error names should be descriptive.
+		NoneValue,
+		/// Errors should have helpful documentation associated with them.
+		StorageOverflow,
+	}
+
+	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
+	// These functions materialize as "extrinsics", which are often compared to transactions.
+	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// An example dispatchable that takes a singles value as a parameter, writes the value to
+		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+		#[pallet::call_index(0)]
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
+		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://docs.substrate.io/main-docs/build/origins/
+			let who = ensure_signed(origin)?;
+
+			// Update storage.
+			<Something<T>>::put(something);
+
+			// Emit an event.
+			Self::deposit_event(Event::SomethingStored { something, who });
+			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
+
+		/// An example dispatchable that may throw a custom error.
+		#[pallet::call_index(1)]
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())]
+		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
+			let _who = ensure_signed(origin)?;
+
+			// Read a value from storage.
+			match <Something<T>>::get() {
+				// Return an error if the value has not been set.
+				None => return Err(Error::<T>::NoneValue.into()),
+				Some(old) => {
+					// Increment the value read from storage; will error in the event of overflow.
+					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+					// Update the value in storage with the incremented result.
+					<Something<T>>::put(new);
+					Ok(())
+				},
+			}
+		}
+	}
+
+
+	#[pallet::hooks]
+impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+	/// Offchain worker entry point.
+	///
+	/// By implementing `fn offchain_worker` you declare a new offchain worker.
+	/// This function will be called when the node is fully synced and a new best block is
+	/// successfully imported.
+	/// Note that it's not guaranteed for offchain workers to run on EVERY block, there might
+	/// be cases where some blocks are skipped, or for some the worker runs twice (re-orgs),
+	/// so the code should be able to handle that.
+	fn offchain_worker(block_number: T::BlockNumber) {
+		let signer = Signer::<T, T::AuthorityId>::all_accounts();
+
+	// Using `send_signed_transaction` associated type we create and submit a transaction
+	// representing the call we've just created.
+	// `send_signed_transaction()` return type is `Option<(Account<T>, Result<(), ()>)>`. It is:
+	//	 - `None`: no account is available for sending transaction
+	//	 - `Some((account, Ok(())))`: transaction is successfully sent
+	//	 - `Some((account, Err(())))`: error occurred when sending the transaction
+	let results = signer.send_signed_transaction(|_account| {
+		Call::on_chain_call { key: val }
+	});
+
+	for (acc, res) in &results {
+		match res {
+			Ok(()) => log::info!("[{:?}]: submit transaction success.", acc.id),
+			Err(e) => log::error!("[{:?}]: submit transaction failure. Reason: {:?}", acc.id, e),
+		}
+	}
+
+	Ok(())
+		// The entry point of your code called by offchain worker
+	}
+	// ...
+}
+/// This pallet's configuration trait
+#[pallet::config]
+pub trait Config: CreateSignedTransaction<Call<Self>> + frame_system::Config {
+	// ...
+	type AuthorityId: AppCrypto<Self::Public, Self::Signature>;
+}
+}
